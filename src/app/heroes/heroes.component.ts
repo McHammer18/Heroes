e8392fb8e4cad668dfe43b90b643e6a937bfe639
Morgan Christensen <mchr335@wgu.edu>
2023-06-07 15:29:46 -0500

    add details popup to go with buttons

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,8 +8,9 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
+  }
 }
