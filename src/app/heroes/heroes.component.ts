69d5518bde2e8750a0099485ec473097c4b8c605
Morgan Christensen <mchr335@wgu.edu>
2023-06-07 16:09:55 -0500

    Add Complete service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +8,16 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+  heroes: Hero[] = [];
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   selectedHero?: Hero;
   onSelect(hero:Hero): void{
     this.selectedHero = hero;
+  }
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
